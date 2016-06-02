1988cf154c48f6e162859ba3a0374ca97a39df8e
Alan Hardman <ahardman@thrivelife.com>
2016-06-02 15:57:27 -0600

    Adding some todos

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*     __            _    __
+  ___ / /___ _____  (_)__/ /__  _______ __ ____ __
+ (_-</ __/ // / _ \/ / _  / _ \/ __/ _ \\ \ / // /
+/___/\__/\_,_/ .__/_/\_,_/ .__/_/  \___/_\_\\_, /
+            /_/         /_/                /___/*/
+/**
+ * @author Alan Hardman <alan@phpizza.com>
+ *
+ * @version 0.0.0
+ *
+ * @todo  Support "host" configuration file to allow stuff like this:
+ *        http://example.com/index.php <- this file
+ *        http://other.example.org/asdf <- proxied page
+ *        http://example.com/asdf <- user-given URL
+ *
+ * @todo  Cache request responses and return last-cached copy of response
+ */
